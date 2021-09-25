fdfac4e038ed2b6946457015d27de109a0281e8a
lingsat <lingsat@bigmir.net>
2021-09-25 12:15:37 +0300

    Creating Git Repository

---
+++ script/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $('.content__slider').slick({   
+      dots: true,           
+      autoplay: true,
+      autoplaySpeed: 4600
+    });
+  });
+
+$(document).ready(function(){
+  $('.fast__slider').slick({   
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    swipeToSlide: true
+  });
+});
+
+$(document).ready(function(){
+  $('.top__slider').slick({ 
+        
+  });
+});
